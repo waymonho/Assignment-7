2cd8947d5b062fc30bdf356ddb7b3e9a8e165e1c
waymonho <waymonho@csu.fullerton.edu>
2017-04-05 19:57:36 -0700

    Chapters 13 + Silver Challenge, JSON-Server + RemoteDataStore.js, Chapter 14

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,25 +1,36 @@
 (function(window) {
     'use strict';
     var App = window.App || {};
+    var Promise = window.Promise;
 
     function DataStore() {
+        console.log('running the DataStore functon');
         this.data = {};
+    }
+
+    function promiseResolvedWith(value) {
+        var promise = new Promise(function(resolve, reject) {
+            resolve(value);
+        });
+        return promise;
     }
 
     DataStore.prototype.add = function(key, val) {
         this.data[key] = val;
+        return promiseResolvedWith(null);
     };
 
     DataStore.prototype.get = function(key) {
-        return this.data[key];
+        return promiseResolvedWith(this.data[key]);
     };
 
     DataStore.prototype.getAll = function() {
-        return this.data;
+        return promiseResolvedWith(this.data);
     };
 
     DataStore.prototype.remove = function(key) {
         delete this.data[key];
+        return promiseResolvedWith(null);
     };
 
     App.DataStore = DataStore;
